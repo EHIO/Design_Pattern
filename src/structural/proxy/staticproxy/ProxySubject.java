117fa89bc4d4ebb762dfc44e3c722b7c7a2ed227
Run <1160061218@qq.com>
2019-01-14 17:35:28 +0800

    clean up

--- src/structural/proxy/staticproxy/ProxySubject.java
+++ src/structural/proxy/staticproxy/ProxySubject.java
@@ -1,9 +1,5 @@
 package structural.proxy.staticproxy;
 
-/**
- * 代理角色(ProxySubject)：代理对象保存了一个真实引用，从而可以操作真实对象，并提供一个与Subject的接口相同的接口，
- * 从而在任何时刻都能代替真实对象。同时，代理对象可以在执行真实对象操作时，附加其他的操作，相当于对真实对象进行封装。
- */
 public class ProxySubject extends Subject {
     private RealSubject realSubject;
 
