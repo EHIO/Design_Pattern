2c9901ba3bf637fc4e8c6624cd31dee2b32808f6
Run <1160061218@qq.com>
2019-01-17 17:08:55 +0800

    clean up

--- src/creational/factory/factory_method/example_2/ChicagoStylePizzaStore.java
+++ src/creational/factory/factory_method/example_2/ChicagoStylePizzaStore.java
@@ -12,15 +12,10 @@ public class ChicagoStylePizzaStore extends PizzaStore {
     protected Pizza createPizza(String type) {
         if ("cheese".equals(type)) {
             return new ChicagoStyleCheesePizza();
-        } else if ("pepperoniPizza".equals(type)) {
-            return new ChicagoStylePepperoniPizza();
-        } else if ("clam".equals(type)) {
-            return new ChicagoStyleClamPizza();
         } else if ("veggie".equals(type)) {
             return new ChicagoStyleVeggiePizza();
         } else {
             return null;
         }
     }
-
 }
--- src/creational/factory/factory_method/example_2/NYStylePizzaStore.java
+++ src/creational/factory/factory_method/example_2/NYStylePizzaStore.java
@@ -12,15 +12,10 @@ public class NYStylePizzaStore extends PizzaStore {
     protected Pizza createPizza(String type) {
         if ("cheese".equals(type)) {
             return new NYStyleCheesePizza();
-        } else if ("pepperoniPizza".equals(type)) {
-            return new NYStylePepperoniPizza();
-        } else if ("clam".equals(type)) {
-            return new NYStyleClamPizza();
         } else if ("veggie".equals(type)) {
             return new NYStyleVeggiePizza();
         } else {
             return null;
         }
     }
-
 }
