e5e6ca3fc4f884274f15592234e8c4fd8013c3a8
Run <1160061218@qq.com>
2019-04-17 15:25:32 +0800

    clean up

--- src/structural/proxy/javaproxy/UserTest.java
+++ src/structural/proxy/javaproxy/UserTest.java
@@ -16,7 +16,10 @@ public class UserTest {
          *  2、目标类的实现的所有接口的.class
          *  3、调用处理器
          */
-        UserService proxy = (UserService) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), userService.getClass().getInterfaces(), uih);
+		UserService proxy = (UserService) Proxy.newProxyInstance(
+				Thread.currentThread().getContextClassLoader(),
+				userService.getClass().getInterfaces(),
+				uih);
 //        只有代理对象proxy直接调用的那个方法才是真正的走代理的！
         proxy.test1();
 //        proxy.test2();
